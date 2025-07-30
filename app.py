b0b6c148d8560665a222b0a2bca7dbd858b9670f
poojithabairi <poojithabairi29@gmail.com>
2025-07-30 09:57:26 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
    return "hlo i am pooja"
    
 if __name__=="__main__":
-port=int(os.environ.get("PORT",5000))
-app.run(host='0.0.0.0',port=port)
+ port=int(os.environ.get("PORT",5000))
+ app.run(host='0.0.0.0',port=port)
